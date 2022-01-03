ab86ea08311dfa31901429c98d43d4c4251a969b
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-01-03 10:00:43 -0500

    create CRUD routes for database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //* Imports
 const express = require('express');
+const mysql = require('mysql2');
 
 //* Variables
 const PORT = process.env.PORT || 3000;
@@ -9,16 +10,52 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-//* Routes
+
+//* Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Daniel12',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  
 
 
+//* Routes and Queries
 
+// GET request
+db.query('SELECT * FROM candidates WHERE id = 1', (err,rows)=>{
+    if(err) console.log(err);
+    else console.log(rows);
+});
+
+// DELETE request
+db.query('DELETE FROM candidates where id = ?',1, (err,result)=>{
+    if (err) console.log(err);
+    else console.log(result);
+});
+
+// POST request
+const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)';
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result)=>{
+    if (err) console.log(err);
+    else console.log(result);
+});
 
 // Unsupported routes
 // This must be the last route or it will override the others
 app.use((req,res)=>{
     res.status(404).end();
 });
+
+
 
 //* Listen for server
 app.listen(PORT, ()=>{
